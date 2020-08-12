6d6f6df10a4e18f6e5180599696f392821e8788c
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-08-11 22:42:44 -0400

    finished form and validation

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import * as yup from "yup";
+import axios from "axios";
 
 const Form = () => {
     const [formData, setFormData] = useState({
@@ -10,10 +11,15 @@ const Form = () => {
     })
 
     const [errors, setErrors] = useState({
-
+        name: "",
+        email: "",
+        password: "",
+        terms: ""
     })
 
-    const [buttonDisabled, setButtonDisabled] = useState(true)
+    const [account, setAccount] = useState([]);
+
+    const [buttonDisabled, setButtonDisabled] = useState(true);
 
     const schema = yup.object().shape({
         name: yup.string().required("Name is required.") ,
@@ -22,15 +28,57 @@ const Form = () => {
         terms: yup.boolean().oneOf([true], "Please agree to the Terms and Conditions.")
     })
 
-    const handleChange = (e) => {
+    const validateChange = (e) => {
+        yup
+        .reach(schema, e.target.name)
+        .validate(e.target.value)
+        .then((valid) => {
+            setErrors({
+            ...errors,
+            [e.target.errors]: "",
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            setErrors({
+                ...errors,
+                [e.target.name]: err.errors[0] 
+            })
+        });
+    }
 
+    const handleChange = (e) => {
+        e.persist();
+        const newFormData = {
+            ...formData,
+            [e.target.name]: e.target.type==="checkbox" ? e.target.checked : e.target.value
+        }
+        validateChange(e);
+        setFormData(newFormData);
     }
 
     const submit = (e) => {
-
+        e.preventDefault();
+        axios.post("https://reqres.in/api/users", formData)
+        .then((response) => {
+            setAccount(response.data);
+            setFormData({
+                name: "",
+                email: "",
+                password: "",
+                terms: true
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
     }
 
-
+    useEffect(() => {
+        schema.isValid(formData).then((isValid) => {
+            setButtonDisabled(!isValid);
+        })
+    }, [formData])
 
     return (
         <div>
@@ -39,20 +87,25 @@ const Form = () => {
                 <label htmlFor="name">
                     Name: 
                     <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+                    {errors.name.length > 0 ? <p className="errors">{errors.name}</p> : null}
                 </label>
                 <label htmlFor="email">
                     Email: 
                     <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+                    {errors.email.length > 0 ? <p className="errors">{errors.email}</p> : null}
                 </label>
                 <label htmlFor="password">
                     Password: 
                     <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
+                    {errors.password.length > 0 ? <p className="errors">{errors.password}</p> : null}
                 </label>
                 <label htmlFor="terms">
                     Terms and Conditions: 
-                    <input type="checkbox" id="terms" name="terms" checked={true} onChange={handleChange} />
+                    <input type="checkbox" id="terms" name="terms" checked={formData.terms} onChange={handleChange} />
+                    {errors.terms.length > 0 ? <p className="errors">{errors.terms}</p> : null}
                 </label>
                 <button type="submit" disabled={buttonDisabled} >Create Account</button>
+                <pre>{JSON.stringify(account, null, 2)}</pre>
             </form>
         </div>
     )
